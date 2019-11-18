a1c0250a404ef7fa6936cdbfcde00957e07d89f3
EtricKombat <ericjoy1991@gmail.com>
2019-11-18 07:56:18 +0530

    Adding envv

--- KOBSetUp.sh
+++ KOBSetUp.sh
@@ -121,26 +121,27 @@ echo "Installing KOBPROJECT scripts..."
 
 # Create directory structure
 
-sudo echo "Install scripts..."
+sudo echo "Creating KOB_Dir & exporting KOB_env_Dir"
 sudo mkdir -p /usr/bin/KOB_Dir
 export KOB_env_Dir=/usr/bin/KOB_Dir
+echo "change to KOB_env_Dir"
 cd $KOB_env_Dir
 sudo git clone https://github.com/EtricKombat/KOBDevOps.git
-cd $KOB_env_Dir
-echo "The cursor is here"
+
+echo "This is the present working directory"
 pwd
 sudo mv KOBDevOps/bin/sh/ /usr/bin/
-sudo mv KOBDevOps/bin/env/ /usr/bin/
+sudo mv KOBDevOps/bin/envv/ /usr/bin/
+sudo mv KOBDevOps/KOB /usr/bin
 
 # sudo mv KOBDevOps/bin/env /usr/bin 
 #sudo mv bin/sh bin/env /usr/bin
-#sudo mv $KOB_Dir/KOBDevOps/KOB /usr/bin
 
 
 
 sudo echo " available KOB_ENVs..."
 export KOB_SH=/usr/bin/sh
-export KOB_ENV=/usr/bin/env
+export KOB_ENV=/usr/bin/envv
 
 
 
