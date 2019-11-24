640f812d9b5cc506475d20ae654cbc8c26c87eae
EtricKombat <ericjoy1991@gmail.com>
2019-11-24 12:32:59 +0530

    Adding modified KOBSETUP & Kob

--- KOBSetup.sh
+++ KOBSetup.sh
@@ -95,29 +95,29 @@ else
 fi
 
 
-echo "Installing KOBPROJECT scripts..."
+echo "Uninstalling KOBPROJECT scripts..."
 
 # Removing existing environment variables 
-sudo rm -rf /usr/bin/KOB_Dir/ /usr/bin/sh/ /usr/bin/envv/ /usr/bin/KOB
+sudo rm -rf /usr/bin/KOB_Dir/ /usr/bin/TOB_Dir/ /usr/bin/sh/ /usr/bin/envv/ /usr/bin/KOB
 
 # Create directory structure
 
+sudo echo "Installing KOBPROJECT scripts..."
 sudo echo "Creating KOB_Dir & exporting KOB_env_Dir"
 sudo mkdir -p /usr/bin/KOB_Dir
 export KOB_env_Dir=/usr/bin/KOB_Dir
-echo "change to KOB_env_Dir"
-cd $KOB_env_Dir
-sudo git clone https://github.com/EtricKombat/KOBDevOps.git
-
+sudo echo "Creating TOB_Dir & exporting TOB_env_Dir"
 sudo mkdir -p /usr/bin/TOB_Dir
 export TOB_env_Dir=/usr/bin/TOB_Dir
 
 
+echo "change to KOB_env_Dir"
+cd $KOB_env_Dir
+sudo git clone https://github.com/EtricKombat/KOBDevOps.git
 
-echo "This is the present working directory"
+
+echo "This is the present working directory now..."
 pwd
-
-
 
 
 
@@ -127,13 +127,6 @@ sudo cp -r KOBDevOps/src/bash/KOB /usr/bin
 
 export KOB_SH=/usr/bin/sh
 export KOB_ENV=/usr/bin/envv
-
-
-
-
-
-
-
 
 
 
