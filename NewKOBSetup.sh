c35914ca1cb2608339323271fa7d10c2df785ad1
EtricKombat <ericjoy1991@gmail.com>
2019-12-02 14:21:01 +0530

    Adding latest update on NewKOBSetup.sh

--- NewKOBSetup.sh
+++ NewKOBSetup.sh
@@ -1,50 +1,28 @@
-opyright 2017 Marco Vermeulen
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-
 #Install: stable
 
 # Global variables
-SDKMAN_SERVICE="https://api.sdkman.io/2"
-SDKMAN_VERSION="5.7.4+362"
-SDKMAN_PLATFORM=$(uname)
+KOBDEVOPS_VERSION="0.01"
+KOBDEVOPS_PLATFORM=$(uname)
+mkdir -p $HOME/.kobdevops
 
-if [ -z "$SDKMAN_DIR" ]; then
-    SDKMAN_DIR="$HOME/.sdkman"
+if [ -z "$KOBDEVOPS_DIR" ]; then
+    KOBDEVOPS_DIR="$HOME/.kobdevops"
 fi
 
 # Local variables
-sdkman_bin_folder="${SDKMAN_DIR}/bin"
-sdkman_src_folder="${SDKMAN_DIR}/src"
-sdkman_tmp_folder="${SDKMAN_DIR}/tmp"
-sdkman_stage_folder="${sdkman_tmp_folder}/stage"
-sdkman_zip_file="${sdkman_tmp_folder}/sdkman-${SDKMAN_VERSION}.zip"
-sdkman_ext_folder="${SDKMAN_DIR}/ext"
-sdkman_etc_folder="${SDKMAN_DIR}/etc"
-sdkman_var_folder="${SDKMAN_DIR}/var"
-sdkman_archives_folder="${SDKMAN_DIR}/archives"
-sdkman_candidates_folder="${SDKMAN_DIR}/candidates"
-sdkman_config_file="${sdkman_etc_folder}/config"
-sdkman_bash_profile="${HOME}/.bash_profile"
-sdkman_profile="${HOME}/.profile"
-sdkman_bashrc="${HOME}/.bashrc"
-sdkman_zshrc="${HOME}/.zshrc"
+kobdevops_bin_folder="${KOBDEVOPS_DIR}/bin"
+kobdevops_src_folder="${KOBDEVOPS_DIR}/src"
+kobdevops_env_folder="${KOBDEVOPS_DIR}/tmp"
 
-sdkman_init_snippet=$( cat << EOF
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="$SDKMAN_DIR"
-[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+kobdevops_bash_profile="${HOME}/.bash_profile"
+kobdevops_profile="${HOME}/.profile"
+kobdevops_bashrc="${HOME}/.bashrc"
+kobdevops_zshrc="${HOME}/.zshrc"
+
+kobdevops_init_snippet=$( cat << EOF
+#THIS MUST BE AT THE END OF THE FILE FOR KOBDEVOPS TO WORK!!!
+export KOBDEVOPS_DIR="$KOBDEVOPS_DIR"
+[[ -s "${KOBDEVOPS_DIR}/bin/kobdevops-init.sh" ]] && source "${KOBDEVOPS_DIR}/bin/kobdevops-init.sh"
 EOF
 )
 
@@ -67,66 +45,28 @@ case "$(uname)" in
         freebsd=true
 esac
 
-
-echo ''
-echo '                                -+syyyyyyys:'
-echo '                            `/yho:`       -yd.'
-echo '                         `/yh/`             +m.'
-echo '                       .oho.                 hy                          .`'
-echo '                     .sh/`                   :N`                `-/o`  `+dyyo:.'
-echo '                   .yh:`                     `M-          `-/osysoym  :hs` `-+sys:      hhyssssssssy+'
-echo '                 .sh:`                       `N:          ms/-``  yy.yh-      -hy.    `.N-````````+N.'
-echo '               `od/`                         `N-       -/oM-      ddd+`     `sd:     hNNm        -N:'
-echo '              :do`                           .M.       dMMM-     `ms.      /d+`     `NMMs       `do'
-echo '            .yy-                             :N`    ```mMMM.      -      -hy.       /MMM:       yh'
-echo '          `+d+`           `:/oo/`       `-/osyh/ossssssdNMM`           .sh:         yMMN`      /m.'
-echo '         -dh-           :ymNMMMMy  `-/shmNm-`:N/-.``   `.sN            /N-         `NMMy      .m/'
-echo '       `oNs`          -hysosmMMMMydmNmds+-.:ohm           :             sd`        :MMM/      yy'
-echo '      .hN+           /d:    -MMMmhs/-.`   .MMMh   .ss+-                 `yy`       sMMN`     :N.'
-echo '     :mN/           `N/     `o/-`         :MMMo   +MMMN-         .`      `ds       mMMh      do'
-echo '    /NN/            `N+....--:/+oooosooo+:sMMM:   hMMMM:        `my       .m+     -MMM+     :N.'
-echo '   /NMo              -+ooooo+/:-....`...:+hNMN.  `NMMMd`        .MM/       -m:    oMMN.     hs'
-echo '  -NMd`                                    :mm   -MMMm- .s/     -MMm.       /m-   mMMd     -N.'
-echo ' `mMM/                                      .-   /MMh. -dMo     -MMMy        od. .MMMs..---yh'
-echo ' +MMM.                                           sNo`.sNMM+     :MMMM/        sh`+MMMNmNm+++-'
-echo ' mMMM-                                           /--ohmMMM+     :MMMMm.       `hyymmmdddo'
-echo ' MMMMh.                  ````                  `-+yy/`yMMM/     :MMMMMy       -sm:.``..-:-.`'
-echo ' dMMMMmo-.``````..-:/osyhddddho.           `+shdh+.   hMMM:     :MmMMMM/   ./yy/` `:sys+/+sh/'
-echo ' .dMMMMMMmdddddmmNMMMNNNNNMMMMMs           sNdo-      dMMM-  `-/yd/MMMMm-:sy+.   :hs-      /N`'
-echo '  `/ymNNNNNNNmmdys+/::----/dMMm:          +m-         mMMM+ohmo/.` sMMMMdo-    .om:       `sh'
-echo '     `.-----+/.`       `.-+hh/`         `od.          NMMNmds/     `mmy:`     +mMy      `:yy.'
-echo '           /moyso+//+ossso:.           .yy`          `dy+:`         ..       :MMMN+---/oys:'
-echo '         /+m:  `.-:::-`               /d+                                    +MMMMMMMNh:`'
-echo '        +MN/                        -yh.                                     `+hddhy+.'
-echo '       /MM+                       .sh:'
-echo '      :NMo                      -sh/'
-echo '     -NMs                    `/yy:'
-echo '    .NMy                  `:sh+.'
-echo '   `mMm`               ./yds-'
-echo '  `dMMMmyo:-.````.-:oymNy:`'
-echo '  +NMMMMMMMMMMMMMMMMms:`'
-echo '    -+shmNMMMNmdy+:`'
-echo ''
-echo ''
-echo '                                                                 Now attempting installation...'
-echo ''
-echo ''
+echo "     __ ______  ____       _____      __           "
+echo "    / //_/ __ \/ __ )     / ___/___  / /___  ______ "
+echo "   / ,< / / / / __  |_____\__ \/ _ \/ __/ / / / __ \ "
+echo "  / /| / /_/ / /_/ /_____/__/ /  __/ /_/ /_/ / /_/ / "
+echo " /_/ |_\____/_____/     /____/\___/\__/\__,_/  ___/  "
+echo "                                           /_/       "
 
 # Sanity checks
 
-echo "Looking for a previous installation of SDKMAN..."
-if [ -d "$SDKMAN_DIR" ]; then
-	echo "SDKMAN found."
+echo "Looking for a previous installation of KOBDEVOPS..."
+if [ -d "$KOBDEVOPS_DIR" ]; then
+	echo "KOBDEVOPS found."
 	echo ""
 	echo "======================================================================================================"
-	echo " You already have SDKMAN installed."
-	echo " SDKMAN was found at:"
+	echo " You already have KOBDEVOPS installed."
+	echo " KOBDEVOPS was found at:"
 	echo ""
-	echo "    ${SDKMAN_DIR}"
+	echo "    ${KOBDEVOPS_DIR}"
 	echo ""
 	echo " Please consider running the following if you need to upgrade."
 	echo ""
-	echo "    $ sdk selfupdate force"
+	echo "    $ kob selfupdate force"
 	echo ""
 	echo "======================================================================================================"
 	echo ""
@@ -178,7 +118,7 @@ if [[ "$solaris" == true ]]; then
 		echo "======================================================================================================"
 		echo " Please install gsed on your solaris system."
 		echo ""
-		echo " SDKMAN uses gsed extensively."
+		echo " KOBDEVOPS uses gsed extensively."
 		echo ""
 		echo " Restart after installing gsed."
 		echo "======================================================================================================"
@@ -201,99 +141,60 @@ else
 fi
 
 
-echo "Installing SDKMAN scripts..."
+echo "Installing KOBDEVOPS scripts..."
 
 
 # Create directory structure
 
 echo "Create distribution directories..."
-mkdir -p "$sdkman_bin_folder"
-mkdir -p "$sdkman_src_folder"
-mkdir -p "$sdkman_tmp_folder"
-mkdir -p "$sdkman_stage_folder"
-mkdir -p "$sdkman_ext_folder"
-mkdir -p "$sdkman_etc_folder"
-mkdir -p "$sdkman_var_folder"
-mkdir -p "$sdkman_archives_folder"
-mkdir -p "$sdkman_candidates_folder"
+mkdir -p "$kobdevops_bin_folder"
+mkdir -p "$kobdevops_src_folder"
+mkdir -p "$kobdevops_env_folder"
 
-echo "Getting available candidates..."
-SDKMAN_CANDIDATES_CSV=$(curl -s "${SDKMAN_SERVICE}/candidates/all")
-echo "$SDKMAN_CANDIDATES_CSV" > "${SDKMAN_DIR}/var/candidates"
 
-echo "Prime the config file..."
-touch "$sdkman_config_file"
-echo "sdkman_auto_answer=false" >> "$sdkman_config_file"
-echo "sdkman_auto_selfupdate=false" >> "$sdkman_config_file"
-echo "sdkman_insecure_ssl=false" >> "$sdkman_config_file"
-echo "sdkman_curl_connect_timeout=7" >> "$sdkman_config_file"
-echo "sdkman_curl_max_time=10" >> "$sdkman_config_file"
-echo "sdkman_beta_channel=false" >> "$sdkman_config_file"
-echo "sdkman_debug_mode=false" >> "$sdkman_config_file"
-echo "sdkman_colour_enable=true" >> "$sdkman_config_file"
 
-echo "Download script archive..."
-curl --location --progress-bar "${SDKMAN_SERVICE}/broker/download/sdkman/install/${SDKMAN_VERSION}/${SDKMAN_PLATFORM}" > "$sdkman_zip_file"
-
-ARCHIVE_OK=$(unzip -qt "$sdkman_zip_file" | grep 'No errors detected in compressed data')
-if [[ -z "$ARCHIVE_OK" ]]; then
-	echo "Downloaded zip archive corrupt. Are you connected to the internet?"
-	echo ""
-	echo "If problems persist, please ask for help on our Slack:"
-	echo "* easy sign up: https://slack.sdkman.io/"
-	echo "* report on channel: https://sdkman.slack.com/app_redirect?channel=user-issues"
-	rm -rf "$SDKMAN_DIR"
-	exit 2
-fi
 
 echo "Extract script archive..."
 if [[ "$cygwin" == 'true' ]]; then
 	echo "Cygwin detected - normalizing paths for unzip..."
-	sdkman_zip_file=$(cygpath -w "$sdkman_zip_file")
-	sdkman_stage_folder=$(cygpath -w "$sdkman_stage_folder")
+	kobdevops_zip_file=$(cygpath -w "$kobdevops_zip_file")
+	kobdevops_stage_folder=$(cygpath -w "$kobdevops_stage_folder")
 fi
-unzip -qo "$sdkman_zip_file" -d "$sdkman_stage_folder"
+unzip -qo "$kobdevops_zip_file" -d "$kobdevops_stage_folder"
 
-
-echo "Install scripts..."
-mv "${sdkman_stage_folder}/sdkman-init.sh" "$sdkman_bin_folder"
-mv "$sdkman_stage_folder"/sdkman-* "$sdkman_src_folder"
-
-echo "Set version to $SDKMAN_VERSION ..."
-echo "$SDKMAN_VERSION" > "${SDKMAN_DIR}/var/version"
 
 
 if [[ $darwin == true ]]; then
-  touch "$sdkman_bash_profile"
+  touch "$kobdevops_bash_profile"
   echo "Attempt update of login bash profile on OSX..."
-  if [[ -z $(grep 'sdkman-init.sh' "$sdkman_bash_profile") ]]; then
-    echo -e "\n$sdkman_init_snippet" >> "$sdkman_bash_profile"
-    echo "Added sdkman init snippet to $sdkman_bash_profile"
+  if [[ -z $(grep 'kobdevops-init.sh' "$kobdevops_bash_profile") ]]; then
+    echo -e "\n$kobdevops_init_snippet" >> "$kobdevops_bash_profile"
+    echo "Added kobdevops init snippet to $kobdevops_bash_profile"
   fi
 else
   echo "Attempt update of interactive bash profile on regular UNIX..."
-  touch "${sdkman_bashrc}"
-  if [[ -z $(grep 'sdkman-init.sh' "$sdkman_bashrc") ]]; then
-      echo -e "\n$sdkman_init_snippet" >> "$sdkman_bashrc"
-      echo "Added sdkman init snippet to $sdkman_bashrc"
+  touch "${kobdevops_bashrc}"
+  if [[ -z $(grep 'kobdevops-init.sh' "$kobdevops_bashrc") ]]; then
+      echo -e "\n$kobdevops_init_snippet" >> "$kobdevops_bashrc"
+      echo "Added kobdevops init snippet to $kobdevops_bashrc"
   fi
 fi
 
 echo "Attempt update of zsh profile..."
-touch "$sdkman_zshrc"
-if [[ -z $(grep 'sdkman-init.sh' "$sdkman_zshrc") ]]; then
-    echo -e "\n$sdkman_init_snippet" >> "$sdkman_zshrc"
-    echo "Updated existing ${sdkman_zshrc}"
+touch "$kobdevops_zshrc"
+if [[ -z $(grep 'kobdevops-init.sh' "$kobdevops_zshrc") ]]; then
+    echo -e "\n$kobdevops_init_snippet" >> "$kobdevops_zshrc"
+    echo "Updated existing ${kobdevops_zshrc}"
 fi
 
 echo -e "\n\n\nAll done!\n\n"
 
 echo "Please open a new terminal, or run the following in the existing one:"
 echo ""
-echo "    source \"${SDKMAN_DIR}/bin/sdkman-init.sh\""
+echo "    source \"${KOBDEVOPS_DIR}/bin/kobdevops-init.sh\""
 echo ""
 echo "Then issue the following command:"
 echo ""
-echo "    sdk help"
+echo "    kob help"
 echo ""
 echo "Enjoy!!!"
