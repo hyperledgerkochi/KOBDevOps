fc826a1c2b5f27f7f27b37cd8d5ac4a51c6537f6
EtricKombat <eric.joy@wipro.com>
2019-12-18 02:48:56 +0530

    Dec-13-Friday

--- tmp/kobman-tob.sh
+++ tmp/kobman-tob.sh
@@ -1,26 +1,14 @@
 #!/bin/bash 
                                                               
                                                               
-echo "   ________         ____             ____              __  "
-echo "  /_  __/ /_  ___  / __ \_________ _/ __ )____  ____  / /__"
-echo "   / / / __ \/ _ \/ / / / ___/ __  / __  / __ \/ __ \/ //_/"
-echo "  / / / / / /  __/ /_/ / /  / /_/ / /_/ / /_/ / /_/ / ,<   "
-echo " /_/ /_/ /_/\___/\____/_/   \__, /_____/\____/\____/_/|_|  "
-echo "                           /____/                          "
 
 
 function __kobman_tob_build
 {
            
-echo "     ____        _ __    ___                   "
-echo "    / __ )__  __(_) /___/ (_)___  ____ _       "
-echo "   / __  / / / / / / __  / / __ \/ __  /       "
-echo "  / /_/ / /_/ / / / /_/ / / / / / /_/ /  _ _ _ "
-echo " /_____/\__,_/_/_/\__,_/_/_/ /_/\__, /  (_|_|_) "
-echo "                               /____/           "
 
-
-		cd $TOB_env_Dir
+		figlet Building TheOrgBook
+		cd ${KOBMAN_CANDIDATES_DIR}
          	sudo echo "Setting-up TOB instance in your system?"
                 sudo git clone https://github.com/bcgov/TheOrgBook.git
                 sudo wget --no-proxy https://github.com/openshift/source-to-image/releases/download/v1.1.14/source-to-image-v1.1.14-874754de-linux-amd64.tar.gz
@@ -36,15 +24,10 @@ echo "                               /____/           "
 function __kobman_tob_start
 {
 
-echo "    _____ __             __  _                  "
-echo "   / ___// /_____ ______/ /_(_)___  ____ _      "
-echo "   \__ \/ __/ __  / ___/ __/ / __ \/ __  /       "
-echo "  ___/ / /_/ /_/ / /  / /_/ / / / / /_/ /  _ _ _"
-echo " /____/\__/\__,_/_/   \__/_/_/ /_/\__, /  (_|_|_) "
-echo "                                 /____/           "
 
 
-	cd $TOB_env_Dir
+	figlet starting TheOrgBook
+	cd ${KOBMAN_CANDIDATES_DIR}
 	sudo read -p "Do you want to start TOB instance in your system?" reply
         if [ "$reply" = "y" ] || [ "$reply" = "Y" ] || [ "$reply" = "yes" ] || [ "$reply" = "YES" ];
         then
@@ -54,7 +37,8 @@ echo "                                 /____/           "
 
 function __kobman_tob_uninstall
 {
-	cd $TOB_env_Dir
+	figlet Removing TheOrgBook
+	cd ${KOBMAN_CANDIDATES_DIR}
 	sudo TheOrgBook/docker/manage rm
 	sudo rm -rf /usr/local/bin/s2i /usr/local/bin/sti TheOrgBook/	
 }	
--- tmp/kobman-tobdflow.sh
+++ tmp/kobman-tobdflow.sh
@@ -1,14 +1,6 @@
 #!/bin/bash 
 
 
-echo "                                ___       __    __ "
-echo "    ____ _________  ___  ____  / (_)___ _/ /_  / /_ "
-echo "   / __  / ___/ _ \/ _ \/ __ \/ / / __  / __ \/ __/ "
-echo "  / /_/ / /  /  __/  __/ / / / / / /_/ / / / / /_  "
-echo "  \__, /_/   \___/\___/_/ /_/_/_/\__, /_/ /_/\__/  "
-echo " /____/                         /____/             "
-
-
 function __kobman_greenlight_build
 {
 
@@ -19,7 +11,8 @@ echo "  / /_/ / /_/ / / / /_/ / / / / / /_/ /  _ _ _ "
 echo " /_____/\__,_/_/_/\__,_/_/_/ /_/\__, /  (_|_|_) "
 echo "                               /____/           "
 
-		cd $TOB_env_Dir
+		figlet Building greenlight
+ 		cd ${KOBMAN_CANDIDATES_DIR}
                 sudo git clone https://github.com/bcgov/greenlight.git
                 sudo wget --no-proxy https://github.com/openshift/source-to-image/releases/download/v1.1.14/source-to-image-v1.1.14-874754de-linux-amd64.tar.gz
                 sudo tar -xvzf source-to-image-v1.1.14-874754de-linux-amd64.tar.gz
@@ -34,14 +27,9 @@ echo "                               /____/           "
 function __kobman_greenlight_start
 {
 
-echo "    _____ __             __  _                  "
-echo "   / ___// /_____ ______/ /_(_)___  ____ _      "
-echo "   \__ \/ __/ __  / ___/ __/ / __ \/ __  /       "
-echo "  ___/ / /_/ /_/ / /  / /_/ / / / / /_/ /  _ _ _"
-echo " /____/\__/\__,_/_/   \__/_/_/ /_/\__, /  (_|_|_) "
-echo "                                 /____/           "
 
-	cd $TOB_env_Dir
+	figlet starting greenlight
+ 	cd ${KOBMAN_CANDIDATES_DIR}
         sudo read -p "Start KOBDflow instance in your system?" reply
         if [ "$reply" = "y" ] || [ "$reply" = "Y" ] || [ "$reply" = "yes" ] || [ "$reply" = "YES" ];
         then
@@ -52,7 +40,8 @@ echo "                                 /____/           "
 function __kobman_greenlight_uninstall
 {
 	
-	cd $TOB_env_Dir
+	figlet Removing greenlight
+ 	cd ${KOBMAN_CANDIDATES_DIR}
 	sudo greenlight/docker/manage rm
 	sudo rm -rf greenlight/ /usr/local/bin/sti /usr/local/bin/s2i source-to-image-v1.1.14-874754de-linux-amd64.tar.gz	
 }
